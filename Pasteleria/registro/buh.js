f2ff93bd4b819de922c6d49804f923adfc32f1d0
gmiranda11 <ga.mirandar@duocuc.cl>
2025-09-08 14:51:30 -0300

    Add files via upload

---
+++ Pasteleria/registro/buh.js
@@ -0,0 +1,10 @@
+function validar() {
+    let nombre = document.getElementById("nombre").value;
+    if (nombre === "") {
+    alert("El campo nombre no puede estar vacío");
+    } else {
+    alert("Formulario válido");
+    }
+    }
+
+    
